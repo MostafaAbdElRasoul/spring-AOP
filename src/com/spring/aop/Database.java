2d48a7af9ab5e5a0536d53ff55bfae0e16744abc
mostafa <mosta.abdo250@gmail.com>
2022-02-09 23:24:36 +0200

    create @Pointcut

--- src/com/spring/aop/Database.java
+++ src/com/spring/aop/Database.java
@@ -2,12 +2,15 @@ package com.spring.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Database {
 	
+	@Pointcut("execution(public * com.spring.dao.*.*(..))")
+	public void myPointCut() {}
 	//@Before("execution(void com.spring.dao.Post.getPosts())")
 	//@Before("execution(public void com.spring.dao.Post.getPosts())")
 	//@Before("execution(public void getPosts())")
@@ -15,8 +18,13 @@ public class Database {
 	//@Before("execution(public * get*(com.spring.dao.Post))")
 	//@Before("execution(public * get*())")
 	//@Before("execution(public * get*(..))")
-	@Before("execution(public * com.spring.dao.*.*(..))")
+	@Before("myPointCut()")
 	public void connectionDb() {
 		System.out.println("connected");
 	}
+	@Before("myPointCut()")
+	public void login() {
+		System.out.println("login success ..");
+	}
+	
 }
